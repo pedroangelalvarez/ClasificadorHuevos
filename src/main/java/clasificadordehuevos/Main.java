af449740e076946bb847f922b54110c849ebb050
pedroangelalvarez <pedroangel.alvarez24@gmail.com>
2021-12-30 00:40:21 -0500

    Initial commit

---
+++ src/main/java/clasificadordehuevos/Main.java
@@ -0,0 +1,12 @@
+package clasificadordehuevos;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
